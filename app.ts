f12c974186934f762438e26a8fa7378053e9d15e
THUNDER MASTER <thndrmaster@gmail.com>
2021-09-23 11:44:14 -0300

    Inferencia de Tipo Parte 1

--- app.ts
+++ app.ts
@@ -1,2 +1,9 @@
-let ola = (nome: string, sobrenome: string) => console.log("Hello " + nome + sobrenome)
-ola("Alexandre", "Sampaio")
+/*** Inferencia de Tipos ** */
+let mensagem: string= "Iniciando o Estudo de TypeScript!!!"
+let temporadasNarutoShipudden: number=21
+let fazendoOCursoDeAngular: boolean=true
+
+/*** Inferencia de Tipos em Arrays** */
+let listaDeFrutas: Array<string>=['Morango', 'Kiwi', 'Laranja', 'Abacaxi']
+let listaDeDoces: string[]=['Jujuba', 'Doce de Coco', 'Bolo Amélia', 'Doce de Abobora']
+let rating: Array<number>=[0,1,2,3,4,5]
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,9 @@
 "use strict";
-var ola = function (nome, sobrenome) { return console.log("Hello " + nome + sobrenome); };
-ola("Alexandre", "Sampaio");
+/*** Inferencia de Tipos ** */
+var mensagem = "Iniciando o Estudo de TypeScript!!!";
+var temporadasNarutoShipudden = 21;
+var fazendoOCursoDeAngular = true;
+/*** Inferencia de Tipos em Arrays** */
+var listaDeFrutas = ['Morango', 'Kiwi', 'Laranja', 'Abacaxi'];
+var listaDeDoces = ['Jujuba', 'Doce de Coco', 'Bolo Amélia', 'Doce de Abobora'];
+var rating = [0, 1, 2, 3, 4, 5];
